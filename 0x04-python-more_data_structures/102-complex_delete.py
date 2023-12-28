e9fff70a621c5e5a7eb0def1adac604c3fbb178b
robert2030 <robert7ashioya.com>
2023-12-28 04:58:13 -0800

    extra

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,5 @@
+def complex_delete(a_dictionary, value):
+    for idx in list(a_dictionary.keys()):
+        if a_dictionary[idx] == value:
+            del a_dictionary[idx]
+    return a_dictionary
