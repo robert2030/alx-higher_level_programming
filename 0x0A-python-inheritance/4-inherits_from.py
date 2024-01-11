52df1d8024d5e5b27398140ba8b89e5741a700bb
robert2030 <robert7ashioya.com>
2024-01-11 03:47:17 -0800

    inherit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+test
+"""
+
+
+def inherits_from(obj, a_class):
+    """test2"""
+    if type(obj) is not a_class and isinstance(obj, a_class):
+        return True
+    else:i
