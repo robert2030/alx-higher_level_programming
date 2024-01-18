b4edc19a86bca1876d7cdf41563d02b92c7c8359
robert2030 <robert7ashioya.com>
2024-01-18 03:59:26 -0800

    say

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""3main"""
+
+
 def say_my_name(first_name, last_name=""):
+    """main"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
