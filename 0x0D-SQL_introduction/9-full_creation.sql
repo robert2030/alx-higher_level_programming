3b9489b2cd3499c9c16043e473e2a3fc9fe573c3
robert2030 <robert7ashioya.com>
2024-02-22 03:40:41 -0800

    creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- full creation
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES (id = 1, name = “John”, score = 10);
-INSERT INTO second_table (id, name, score) VALUES (id = 2, name = “Alex”, score = 3);
-INSERT INTO second_table (id, name, score) VALUES (id = 3, name = “Bob”, score = 14);
-INSERT INTO second_table (id, name, score) VALUES (id = 4, name = “George”, score = 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table `(id, name, score)` VALUES (id = 1, name = “John”, score = 10);
+INSERT INTO second_table `(id, name, score)` VALUES (id = 2, name = “Alex”, score = 3);
+INSERT INTO second_table `(id, name, score)` VALUES (id = 3, name = “Bob”, score = 14);
+INSERT INTO second_table `(id, name, score)` VALUES (id = 4, name = “George”, score = 8);
 
