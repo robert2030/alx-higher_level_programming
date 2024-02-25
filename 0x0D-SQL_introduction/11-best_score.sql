43a4b1c8ffc87acd7a456dd6f520983849aaaa6c
robert2030 <robert7ashioya.com>
2024-02-24 22:03:09 -0800

    11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,3 @@
--- list all records
-SELECT * FROM second_table WHERE score >= 10;
+-- List the score greather than or equal to 10
+-- Execute: cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
